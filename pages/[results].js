aebfb01317793b11267b169d933d00e0dca69f1e
MarlonMorales232409 <marlon.morales2324@icloud.com>
2022-03-14 03:46:10 +0100

    fix an error

--- pages/[results].js
+++ pages/[results].js
@@ -4,15 +4,14 @@ import Header from '../components/Header'
 import ResultData from '../components/resultData'
 
 const Results = () => {
-    const key = useRouter().query.results
+  const key = useRouter().query.results
 
-   
-    return (
-        <div>
-          <Header />
-            <ResultData url={key}/>
-        </div>
-      )
+  return (
+    <div>
+      <Header />
+      <ResultData url={key} />
+    </div>
+  )
 }
 
-export default Results;
+export default Results
