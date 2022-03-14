ed89b3c936055a7a263f1214a763a4d2db1a1346
MarlonMorales232409 <marlon.morales2324@icloud.com>
2022-03-14 03:52:56 +0100

    Error Fixed

--- pages/[results].js
+++ pages/[results].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 import Header from '../components/Header'
-import ResultData from '../components/resultData'
+import ResultData from '../components/ResultData'
 
 const Results = () => {
   const key = useRouter().query.results
